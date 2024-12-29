1a90f0972708be31a592fb5588091c0cc7597e68
James <jwatson55987@gmail.com>
2024-12-29 15:32:22 -0600

    added constants and conversion

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,21 @@
+package frc.robot;
+ import java.security.PublicKey;
+
+
+public final class Constants {
+    
+    private Constants() {
+
+    }
+
+    public static final class Wrist {
+        public static final int CONVERSION_FACTOR = 360;
+        public static final int PIVOT_MOTOR_ID = 1;
+        public static final double HIGH = 225;
+        public static final double FLAT = 180;
+        public static final double LOW = 135;
+        public static final double WRIST_P = 2;
+        public static final double WRIST_I = 1;
+        public static final double WRIST_D = 1;
+    }
+}
--- src/main/java/frc/robot/subsystem/Wrist.java
+++ src/main/java/frc/robot/subsystem/Wrist.java
@@ -1,9 +1,13 @@
 package frc.robot.subsystem;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import frc.robot.Constants;
+
+
 
 public class Wrist {
 
@@ -16,14 +20,14 @@ public class Wrist {
     private CANSparkMax pivotMotor;
     private WristStates state;
     private PIDController controller;
+    private RelativeEncoder encoder;    
 
     public Wrist() {
+        pivotMotor = new CANSparkMax(Constants.Wrist.PIVOT_MOTOR_ID, MotorType.kBrushless);
+        encoder.setPositionConversionFactor(Constants.Wrist.CONVERSION_FACTOR); //setting conversion factor
+        encoder.setPosition(0);
+        controller = new PIDController(Constants.Wrist.WRIST_P, Constants.Wrist.WRIST_I, Constants.Wrist.WRIST_D); //not accurate
         state = WristStates.FLAT;
-        pivotMotor = new CANSparkMax(1, MotorType.kBrushless);
-
-        pivotMotor.getEncoder().setPosition(0);
-
-        controller = new PIDController(0, 0, 0);
     }
 
     public void setstate(WristStates state) {
@@ -33,16 +37,15 @@ public class Wrist {
     public void periodic() {
         switch (state) {
             case HIGH:
-                pivotMotor.set(controller.calculate(pivotMotor.getEncoder().getPosition(), 225));
-                break;
+                pivotMotor.set(controller.calculate(pivotMotor.getEncoder().getPosition(), Constants.Wrist.HIGH)); //wrist high
+                break; 
             
             case LOW:
-                pivotMotor.set(controller.calculate(pivotMotor.getEncoder().getPosition(), 135));
+                pivotMotor.set(controller.calculate(pivotMotor.getEncoder().getPosition(), Constants.Wrist.LOW)); //wrist low
                 break;
     
             case FLAT:
-                pivotMotor.set(controller.calculate(pivotMotor.getEncoder().getPosition(), 180));
-    
+                pivotMotor.set(controller.calculate(pivotMotor.getEncoder().getPosition(), Constants.Wrist.FLAT)); //wrist straight
                 break;
         
         }
